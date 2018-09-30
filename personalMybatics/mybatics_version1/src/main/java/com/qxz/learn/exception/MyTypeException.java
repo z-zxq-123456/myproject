84bffbe2ad444c89df56c6227efc11ec0cd8811e
1483309150_qq.com_bxbank <1483309150@qq.com>
2018-09-30 15:45:10 +0800

    手写 mybatics 增加typeHandler

---
+++ personalMybatics/mybatics_version1/src/main/java/com/qxz/learn/exception/MyTypeException.java
@@ -0,0 +1,13 @@
+package com.qxz.learn.exception;
+
+/**
+ * @Description :
+ * @Author :zhouxqh
+ * @Date : Create on 2018/9/30
+ */
+public class MyTypeException extends RuntimeException {
+
+    public MyTypeException(String message) {
+        super(message);
+    }
+}
--- personalMybatics/mybatics_version1/src/main/java/com/qxz/learn/type/MyObjectTypeHandler.java
+++ personalMybatics/mybatics_version1/src/main/java/com/qxz/learn/type/MyObjectTypeHandler.java
@@ -1,29 +1,24 @@
 package com.qxz.learn.type;
 
-import java.sql.CallableStatement;
+
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class MyObjectTypeHandler  implements MyTypeHandler{
+public class MyObjectTypeHandler extends MyBaseTypeHandler<Object>{
 
     @Override
-    public void setParameter(PreparedStatement ps, int i, Object parameter, MyJdbcType jdbcType) throws SQLException {
-
+    public void setNonNullParameter(PreparedStatement ps, int i, Object parameter, MyJdbcType jdbcType) throws SQLException {
+        ps.setObject(i,parameter);
     }
 
     @Override
-    public Object getResult(ResultSet rs, String columnName) throws SQLException {
-        return null;
+    public Object getNullableResult(ResultSet rs, String columnName) throws SQLException {
+        return rs.getObject(columnName);
     }
 
     @Override
-    public Object getResult(ResultSet rs, int columnIndex) throws SQLException {
-        return null;
-    }
-
-    @Override
-    public Object getResult(CallableStatement cs, int columnIndex) throws SQLException {
-        return null;
+    public Object getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
+        return rs.getObject(columnIndex);
     }
 }
